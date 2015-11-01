eed2f80bcabf0ea5d6d19be70677da9b6a616b21
John Gedeon <js1@gedeons.com>
2015-11-01 14:55:51 -0800

    Remove comments, who needs those. Push server name and aliases if present

--- bin/start-up.sh
+++ bin/start-up.sh
@@ -3,28 +3,7 @@
 # un-official strict mode
 set -euo pipefail
 
-export CONFIG_REFRESH=${CONFIG_REFRESH:-300}
-
 echo "[nginx] booting container. ETCD: $ETCD."
-
-# Start nginx so we can generate the configs
-# service nginx start
-
-# Make initial configuration every 5 seconds until successful
-# until confd -onetime -node $ETCD; do
-#   echo "[nginx] waiting for confd to create initial nginx configuration."
-#   sleep 5
-# done
-
-# Put a continual polling `confd` process into the background to watch
-# for changes every $CONFIG_REFRESH seconds
-# confd -interval $CONFIG_REFRESH -node $ETCD > /var/log/confd.log 2>&1 &
-# echo "[nginx] confd is now monitoring etcd for changes..."
-
-# # Start the Nginx service using the generated config
-# echo "[nginx] starting nginx service..."
-# # stop nginx started by debian so we can start here in foreground
-# service nginx stop
 
 echo "[nginx] etcd updater is now updating etcd with changes..."
 etcd_updater_service.rb start
--- lib/etcd_updater.rb
+++ lib/etcd_updater.rb
@@ -19,15 +19,33 @@ class EtcdUpdater
 
     @path = "/v2/keys/services/web"
     @path += "/#{@name}" if @name
-    @path += "/#{@hostname}"
+
+    @server_name = ENV["SERVER_NAME"] || @name
+    @server_aliases = ENV["SERVER_ALIASES"] || nil
   end
 
   def update_config
     # Add server ip
     @connection.put do |req|
-      req.url @path
+      req.url "#{@path}/servers/#{@hostname}"
       req.params["value"] = @container_ip
       req.params["ttl"] = @ttl
+    end
+
+    if @server_name
+      @connection.put do |req|
+        req.url "#{@path}/server_name"
+        req.params["value"] = @server_name
+        req.params["ttl"] = @ttl
+      end
+    end
+
+    if @server_aliases
+      @connection.put do |req|
+        req.url "#{@path}/server_aliases"
+        req.params["value"] = @server_aliases
+        req.params["ttl"] = @ttl
+      end
     end
   end
 end
